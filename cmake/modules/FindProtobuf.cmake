c994c7fadb87ce6aebc1199dd0d6e343bd7eefa2
Nick Lu <nikraaaazy@me.com>
2016-08-21 16:51:59 +0800

    Protobuf

--- cmake/modules/FindProtobuf.cmake
+++
@@ -1,8 +0,0 @@
-# - Try to find PROTOBUF
-#
-# The following are set after configuration is done:
-#  PROTOBUF_LIBRARIES
-
-include(FindPackageHandleStandardArgs)
-find_library(PROTOBUF_LIBRARY protobuf)
-set(PROTOBUF_LIBRARIES ${PROTOBUF_LIBRARY})
